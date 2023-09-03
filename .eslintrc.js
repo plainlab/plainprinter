788ba20fcc991d8e919fdda4b09b70b4053a4427
Toxybenzoic <toxybenzoic@proton.me>
2023-09-03 14:48:57 -0400

    Fix windows issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,16 @@ module.exports = {
     'import/no-unresolved': 'error',
     // Since React 17 and typescript 4.1 you can safely disable the rule
     'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'off',
+        jsx: 'off',
+        ts: 'off',
+        tsx: 'off',
+      },
+    ],
   },
   parserOptions: {
     ecmaVersion: 2020,
